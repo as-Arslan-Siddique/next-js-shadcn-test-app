9b9df9078eece3baddf497d88a8caa6301549f1f
Arslan Siddique <arslan@theventurebeyond.com>
2024-09-28 21:06:17 +0500

    testing

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import { TestContent } from "@/app/test/content";
+
+export default function Test() {
+  return (
+    <main className="flex flex-1 flex-col gap-4 p-4 md:p-6 md:pt-0 mt-16 h-full">
+      <TestContent />
+    </main>
+  );
+}
